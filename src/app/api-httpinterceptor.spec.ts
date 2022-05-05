b61927db88e9204d56b1a6db99d69649dce3cb91
Aymeric Azidrou <aymeric.azidrou.auditeur@lecnam.net>
2022-05-05 15:53:04 +0200

    Heroku prep

---
+++ src/app/api-httpinterceptor.spec.ts
@@ -0,0 +1,7 @@
+import { ApiHTTPInterceptor } from './api-httpinterceptor';
+
+describe('ApiHTTPInterceptor', () => {
+  it('should create an instance', () => {
+    expect(new ApiHTTPInterceptor()).toBeTruthy();
+  });
+});
